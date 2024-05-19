50d5eb456d47b8515fbe878da24a185b73266be8
Dedsec007 <nasforger42@gmail.com>
2024-05-19 17:43:47 +0300

    Add DahsboardController + update dashboard.html

---
+++ src/main/java/io/bootify/library/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package io.bootify.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+    
+    @RequestMapping
+    public String dashboard() {
+        return "dashboard";
+    }
+}
